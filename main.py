0948446169b9809841ef48400161ed6b44858d14
Zachary Baum <zac_baum@hotmail.com>
2020-08-17 14:20:59 +0100

    Expand image values to [-1, 1] to match CycleGAN implementation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import skimage.io as iio
 import argparse
 import logging
 import os, time, glob
@@ -11,7 +11,7 @@ import pandas as pd
 from keras.callbacks import CSVLogger, ModelCheckpoint, LearningRateScheduler
 from keras.models import load_model
 from keras.optimizers import Adam
-from skimage.measure import compare_psnr, compare_ssim
+from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 import models
 #from util import *
 
@@ -22,7 +22,7 @@ parser.add_argument('--batch_size', default=1024, type=int, help='batch size')
 parser.add_argument('--train_data', default='./data/npy_data/clean_patches.npy', type=str, help='path of train data')
 parser.add_argument('--test_dir', default='./data/Test/Set68', type=str, help='directory of test dataset')
 parser.add_argument('--sigma', default=10, type=int, help='noise level')
-parser.add_argument('--epoch', default=1, type=int, help='number of train epoches')
+parser.add_argument('--epoch', default=10, type=int, help='number of train epoches')
 parser.add_argument('--lr', default=1e-3, type=float, help='initial learning rate for Adam')
 parser.add_argument('--save_every', default=5, type=int, help='save model at every x epoches')
 parser.add_argument('--pretrain', default=None, type=str, help='path of pre-trained model')
@@ -76,9 +76,7 @@ def train_datagen(y_, batch_size=16):
         np.random.shuffle(indices)    # shuffle
         for i in range(0, len(indices), batch_size):
             ge_batch_y = y_[indices[i:i+batch_size]]
-            noise =  np.random.normal(0, args.sigma/255.0, ge_batch_y.shape)    # noise
-            #noise =  K.random_normal(ge_batch_y.shape, mean=0, stddev=args.sigma/255.0)
-            ge_batch_x = ge_batch_y + noise  # input image = clean image + noise
+            ge_batch_x = np.clip(ge_batch_y + np.random.normal(0, args.sigma/255.0, ge_batch_y.shape), -1, 1)  # input image = clean image + noise
             yield ge_batch_x, ge_batch_y
         
 def train():
@@ -86,6 +84,7 @@ def train():
     data = load_train_data()
     data = data.reshape((data.shape[0],data.shape[1],data.shape[2],1))
     data = data.astype('float32')/255.0
+    data = data * 2 - 1
     # model selection
     if args.pretrain:   model = load_model(args.pretrain, compile=False)
     else:   
@@ -100,7 +99,7 @@ def train():
     lr = LearningRateScheduler(step_decay)
     # train 
     history = model.fit_generator(train_datagen(data, batch_size=args.batch_size),
-                    steps_per_epoch=len(data)//args.batch_size, epochs=args.epoch, verbose=1, 
+                    steps_per_epoch=len(data)//args.batch_size, epochs=args.epoch, verbose=2, 
                     callbacks=[ckpt, csv_logger, lr])
     
     return model
@@ -119,25 +118,28 @@ def test(model):
     for file in file_list:
         # read image
         img_clean = np.array(Image.open(file), dtype='float32') / 255.0
-        img_test = img_clean + np.random.normal(0, args.sigma/255.0, img_clean.shape)
+        img_clean = img_clean * 2 - 1
+        img_test = np.clip(img_clean + np.random.normal(0, args.sigma/255.0, img_clean.shape), -1, 1)
         img_test = img_test.astype('float32')
         # predict
         x_test = img_test.reshape(1, img_test.shape[0], img_test.shape[1], 1) 
         y_predict = model.predict(x_test)
         # calculate numeric metrics
         img_out = y_predict.reshape(img_clean.shape)
-        img_out = np.clip(img_out, 0, 1)
-        psnr_noise, psnr_denoised = compare_psnr(img_clean, img_test), compare_psnr(img_clean, img_out)
-        ssim_noise, ssim_denoised = compare_ssim(img_clean, img_test), compare_ssim(img_clean, img_out)
+        img_out = np.clip(img_out, -1, 1)
+        psnr_noise, psnr_denoised = peak_signal_noise_ratio(img_clean, img_test), peak_signal_noise_ratio(img_clean, img_out)
+        ssim_noise, ssim_denoised = structural_similarity(img_clean, img_test), structural_similarity(img_clean, img_out)
         psnr.append(psnr_denoised)
         ssim.append(ssim_denoised)
         # save images
         filename = file.split('/')[-1].split('.')[0]    # get the name of image file
         name.append(filename)
-        img_test = Image.fromarray((img_test*255).astype('uint8'))
-        img_test.save(out_dir+filename+'_sigma'+'{}_psnr{:.2f}.png'.format(args.sigma, psnr_noise))
-        img_out = Image.fromarray((img_out*255).astype('uint8')) 
-        img_out.save(out_dir+filename+'_psnr{:.2f}.png'.format(psnr_denoised))
+
+        iio.imsave(out_dir+filename+'_sigma'+'{}_psnr{:.2f}.png'.format(args.sigma, psnr_noise), to_range(img_test, 0, 255, np.uint8))
+        iio.imsave(out_dir+filename+'_psnr{:.2f}.png'.format(psnr_denoised), to_range(img_out, 0, 255, np.uint8))
+        img_diff = img_test - img_out
+        img_diff = 2.*(img_diff - np.min(img_diff)) / np.ptp(img_diff) - 1
+        iio.imsave(out_dir+filename+'_diff.png', to_range(img_diff, 0, 255, np.uint8))
     
     psnr_avg = sum(psnr)/len(psnr)
     ssim_avg = sum(ssim)/len(ssim)
@@ -148,6 +150,39 @@ def test(model):
     
     pd.DataFrame({'name':np.array(name), 'psnr':np.array(psnr), 'ssim':np.array(ssim)}).to_csv(out_dir+'/metrics.csv', index=True)
     
+
+def _check(images, dtypes, min_value=-np.inf, max_value=np.inf):
+    # check type
+    assert isinstance(images, np.ndarray), '`images` should be np.ndarray!'
+
+    # check dtype
+    dtypes = dtypes if isinstance(dtypes, (list, tuple)) else [dtypes]
+    assert images.dtype in dtypes, 'dtype of `images` shoud be one of %s!' % dtypes
+
+    # check nan and inf
+    assert np.all(np.isfinite(images)), '`images` contains NaN or Inf!'
+
+    # check value
+    if min_value not in [None, -np.inf]:
+        l = '[' + str(min_value)
+    else:
+        l = '(-inf'
+        min_value = -np.inf
+    if max_value not in [None, np.inf]:
+        r = str(max_value) + ']'
+    else:
+        r = 'inf)'
+        max_value = np.inf
+    assert np.min(images) >= min_value and np.max(images) <= max_value, \
+        '`images` should be in the range of %s!' % (l + ',' + r)
+
+
+def to_range(images, min_value=0.0, max_value=1.0, dtype=None):
+    """Transform images from [-1.0, 1.0] to [min_value, max_value] of dtype."""
+    _check(images, [np.float32, np.float64], -1.0, 1.0)
+    dtype = dtype if dtype else images.dtype
+    return ((images + 1.) / 2. * (max_value - min_value) + min_value).astype(dtype)
+
 if __name__ == '__main__':
 
     print(args.pretrain)
@@ -158,6 +193,6 @@ if __name__ == '__main__':
         test(model)
     else:
         model = train()
-        print(model)
+        model.save('DnCNN_sigma' + str(args.sigma) + '.h5')
         test(model)       
     
